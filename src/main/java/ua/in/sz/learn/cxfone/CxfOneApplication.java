78dcec12e76ca6c52184b9f80f7021ecd1d9f9ac
szolotukhin <szolotukhin@db.com>
2019-09-04 23:07:37 +0300

    CXF

---
+++ src/main/java/ua/in/sz/learn/cxfone/CxfOneApplication.java
@@ -0,0 +1,13 @@
+package ua.in.sz.learn.cxfone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CxfOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CxfOneApplication.class, args);
+    }
+
+}
